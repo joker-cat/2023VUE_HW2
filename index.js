549b1602b7470949b6e54858e5ac6e3ecd3b6b43
joker <jocker.ss.cat@gmail.com>
2024-01-07 01:53:40 +0800

    finish

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ createApp({
         console.log(this.hasCookie);
         console.log(this.pathname);
         if (this.pathname === '/2023VUE_HW2/admin.html' && this.hasCookie === false) {
-            console.log(1);
             location.href = 'index.html';
         }
         if (this.hasCookie === true) {
